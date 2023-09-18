4f81fb04339c476835637ef6e635bfcfdb4269fb
MatiasCalvi <alejandropreuss30@gmail.com>
2023-09-18 18:16:59 -0300

    estructura clases para N piezas

--- problemaDeLasNReinas/elProblemaDeLasNReinas/Piezas/Class1.cs
+++ problemaDeLasNReinas/elProblemaDeLasNReinas/Piezas/Class1.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Piezas
+﻿namespace Piezas
 {
     public class Class1
     {
-        public string ResolverPiezas(IPieza pieza ){
-
-            return "resultado";
-
-        } 
+        public string MostrarSolucion(IPieza pieza)
+        {
+            return "Resuelto";
+        }
     }
 }

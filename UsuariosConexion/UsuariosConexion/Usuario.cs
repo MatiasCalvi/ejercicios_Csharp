d297470ce295647e9e94f1b4ec92c284b11330ef
MatiasCalvi <alejandropreuss30@gmail.com>
2023-09-25 22:22:50 -0300

    conexion a base de datos

---
+++ UsuariosConexion/UsuariosConexion/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsuariosConexion
+{
+    public class Usuario
+    {
+        public int Usuario_Id { get; set; }
+        public string Usuario_Nombre { get; set; }
+        public int Usuario_Edad { get; set; }
+    }
+}

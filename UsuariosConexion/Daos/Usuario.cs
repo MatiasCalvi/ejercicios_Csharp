6b23b1bb1b127161833ff6bad069190e61723898
MatiasCalvi <alejandropreuss30@gmail.com>
2023-09-27 14:12:53 -0300

    Implementacion Crud en el Dao

--- UsuariosConexion/Daos/Usuario.cs
+++ UsuariosConexion/Daos/Usuario.cs
@@ -9,7 +9,7 @@ namespace Daos
     public class Usuario
     {
         public int Usuario_Id { get; set; }
-        public string Usuario_Nombre { get; set; }
+        public string Usuario_Nombre { get; set; } = string.Empty; // ---> se estable como valor predeterminado una cadena vacia
         public int Usuario_Edad { get; set; }
     }
 }
--- UsuariosConexion/UsuariosConexion/Program.cs
+++ UsuariosConexion/UsuariosConexion/Program.cs
@@ -1,20 +1,67 @@
 ﻿using System.Data;
 using Daos;
-class Program
-{
-    static void Main(string[] args)
-    {
-        RepositorioUsuarios repositorio = new RepositorioUsuarios();
 
-        
-        var usuarios = repositorio.ListarTodosLosUsuarios();
-        foreach (var usuario in usuarios) //---------> Listar los usuarios  
+namespace UsuariosConexion
+{
+    class Program
+    {
+        public static Usuario CrearInstanciaUsuario(string nombre, int edad)
         {
-            Console.WriteLine($"ID: {usuario.Usuario_Id}, Nombre: {usuario.Usuario_Nombre}, Edad: {usuario.Usuario_Edad}");
+            return new Usuario
+            {   
+                Usuario_Nombre = nombre,
+                Usuario_Edad = edad,
+            };
         }
 
-        var usuarioInfo = repositorio.ObtenerInformacionDeUnUsuario(3); //--------> Listar usuario por Id
-        Console.WriteLine($"ID: {usuarioInfo.Usuario_Id}, Nombre: {usuarioInfo.Usuario_Nombre}, Edad: {usuarioInfo.Usuario_Edad}");
+
+        static void Main()
+        {
+            RepositorioUsuarios repositorio = new RepositorioUsuarios();
+
+            Usuario usuarioNuevo = CrearInstanciaUsuario("Xelena",22);      
+            // repositorio.CrearUsuario(usuarioNuevo);//-----> Create
+
+
+            var usuarios = repositorio.ListarTodosLosUsuarios();
+            foreach (var usuario in usuarios) //---------> Listar los usuarios  
+            {
+                Console.WriteLine($"ID: {usuario.Usuario_Id}, Nombre: {usuario.Usuario_Nombre}, Edad: {usuario.Usuario_Edad}");
+            }
+
+
+            var usuarioInfo = repositorio.ObtenerInformacionDeUnUsuario(3); //--------> Listar usuario por Id
+            if (usuarioInfo != null) // ---> Verificando nulidad
+            {
+                Console.WriteLine($"ID: {usuarioInfo.Usuario_Id}, Nombre: {usuarioInfo.Usuario_Nombre}, Edad: {usuarioInfo.Usuario_Edad}");
+            }
+            else
+            {
+                Console.WriteLine("No se encontró el usuario."); 
+            }
+
+
+            var usuarioActualizado = repositorio.ObtenerInformacionDeUnUsuario(2);
+            if (usuarioActualizado != null)
+            {
+                usuarioActualizado.Usuario_Nombre = "Lorena";
+                repositorio.ActualizarUsuario(usuarioActualizado); //-----> Update
+            }
+            else
+            {
+                Console.WriteLine("No se encontró el usuario.");
+            }
+
+            var usuarioEliminado = repositorio.ObtenerInformacionDeUnUsuario(5);
+            if (usuarioEliminado != null)
+            {
+                repositorio.EliminarUsuario(usuarioEliminado.Usuario_Id); //-----> Delete
+            }
+            else
+            {
+                Console.WriteLine("No se encontró el usuario.");
+            }
+        }
     }
 }
 

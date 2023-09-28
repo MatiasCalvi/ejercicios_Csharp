c8a1bdd2e59037c47b1e03bc72abced31c39642e
MatiasCalvi <alejandropreuss30@gmail.com>
2023-09-28 18:15:14 -0300

    Modularizacion y reciclado de codigo

--- UsuariosConexion/UsuariosConexion/Program.cs
+++ UsuariosConexion/UsuariosConexion/Program.cs
@@ -1,66 +1,18 @@
-﻿using System.Data;
-using Daos;
+﻿using Daos;
 
 namespace UsuariosConexion
 {
     class Program
     {
-        public static Usuario CrearInstanciaUsuario(string nombre, int edad)
-        {
-            return new Usuario
-            {   
-                Usuario_Nombre = nombre,
-                Usuario_Edad = edad,
-            };
-        }
-
-
         static void Main()
         {
-            RepositorioUsuarios repositorio = new RepositorioUsuarios();
+            RepositorioUsuarios repositorio = new();
 
-            Usuario usuarioNuevo = CrearInstanciaUsuario("Xelena",22);      
-            // repositorio.CrearUsuario(usuarioNuevo);//-----> Create
-
-
-            var usuarios = repositorio.ListarTodosLosUsuarios();
-            foreach (var usuario in usuarios) //---------> Listar los usuarios  
-            {
-                Console.WriteLine($"ID: {usuario.Usuario_Id}, Nombre: {usuario.Usuario_Nombre}, Edad: {usuario.Usuario_Edad}");
-            }
-
-
-            var usuarioInfo = repositorio.ObtenerInformacionDeUnUsuario(3); //--------> Listar usuario por Id
-            if (usuarioInfo != null) // ---> Verificando nulidad
-            {
-                Console.WriteLine($"ID: {usuarioInfo.Usuario_Id}, Nombre: {usuarioInfo.Usuario_Nombre}, Edad: {usuarioInfo.Usuario_Edad}");
-            }
-            else
-            {
-                Console.WriteLine("No se encontró el usuario."); 
-            }
-
-
-            var usuarioActualizado = repositorio.ObtenerInformacionDeUnUsuario(2);
-            if (usuarioActualizado != null)
-            {
-                usuarioActualizado.Usuario_Nombre = "Lorena";
-                repositorio.ActualizarUsuario(usuarioActualizado); //-----> Update
-            }
-            else
-            {
-                Console.WriteLine("No se encontró el usuario.");
-            }
-
-            var usuarioEliminado = repositorio.ObtenerInformacionDeUnUsuario(5);
-            if (usuarioEliminado != null)
-            {
-                repositorio.EliminarUsuario(usuarioEliminado.Usuario_Id); //-----> Delete
-            }
-            else
-            {
-                Console.WriteLine("No se encontró el usuario.");
-            }
+            //repositorio.CrearUsuario("Ximena", 28); //-----> Create
+            repositorio.MostrarInformacionDeUsuario(3); //----> Mostrar usuario por ID
+            repositorio.ActualizarUsuario(10, nombre: "Martin", edad: 23); //-----> Update
+            repositorio.EliminarUsuario(9); //-----> Delete
+            repositorio.MostrarLista(); //-----> Mostrar lista de Usuarios 
         }
     }
 }

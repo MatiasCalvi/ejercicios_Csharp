8466ed447c6fc2da0a58bb8d677da21179a0a087
MatiasCalvi <alejandropreuss30@gmail.com>
2023-10-26 05:12:17 -0300

    recolocacion de bloqueos

---
+++ Kiosco-singleton/Clases/Billetera.cs
@@ -0,0 +1,27 @@
+﻿using Clases.Interfaces;
+
+namespace Clases
+{
+    public class Billetera : IBilletera
+    {
+        public decimal Saldo { get; set; }
+        public Billetera(decimal saldo)
+        {
+            Saldo = saldo;
+        }
+        public bool ValidarSaldo(Producto producto)
+        {
+            return Saldo >= producto.Precio;
+        }
+
+        public void ActualizarSaldo(Producto producto)
+        {
+            if (Saldo - producto.Precio >= 0)
+            {
+                Saldo -= producto.Precio;
+            }
+
+        }
+    }
+
+}
